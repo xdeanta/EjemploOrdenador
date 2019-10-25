a0940298a3105396deff3c4a4e85af395e481113
Xavier De Anta <xdeantabelisario@danielcastelao.org>
2019-10-25 14:17:30 +0200

    Documentacion en el codigo

--- ordenador/Ordenador.java
+++ ordenador/Ordenador.java
@@ -13,17 +13,29 @@ public class Ordenador {
     private Raton rat;
     private Pantalla pant;
     private CPU cpu;
-    
+    /**
+     * Constructor por defecto, construye 3 objetos que la componen
+     */
     public Ordenador(){
         rat=new Raton();
         pant=new Pantalla();
         cpu=new CPU();
     }
+    /**
+     * Constructor parametrizado que recibe como parametros 3 objetos
+     * @param rat Objeto Raton
+     * @param pant Objeto Pantalla
+     * @param cpu Objeto CPU
+     */
     public Ordenador(Raton rat, Pantalla pant, CPU cpu){
         this.rat=rat;
         this.pant=pant;
         this.cpu=cpu;
     }
+    /**
+     * Retorna un objeto tipo raton
+     * @return atributo de la clase
+     */
     public Raton getRaton(){
         return rat;
     }
--- ordenador/Raton.java
+++ ordenador/Raton.java
@@ -11,16 +11,30 @@ package ordenador;
  */
 public class Raton {
     private String tipo;
-    
+    /**
+     * Constructor por defecto, inicializa el atributo tipo en null
+     */
     public Raton(){
         
     }
+    /**
+     * Constructor parametrizado que recibe un string
+     * @param tipo String
+     */
     public Raton(String tipo){
         this.tipo=tipo;
     }
+    /**
+     * retorna un string indicando el tipo de raton
+     * @return tipo String
+     */
     public String getTipo(){
         return tipo;
     }
+    /**
+     * recibe un string y modifica el atributo tipo
+     * @param tipo String
+     */
     public void setTipo(String tipo){
         this.tipo=tipo;
     }
